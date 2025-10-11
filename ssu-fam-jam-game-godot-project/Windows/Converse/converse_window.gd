fa6b873fad13cfdb909e59994d436b302c93021e
AdkinsC0211 <98196686+ConnNAND@users.noreply.github.com>
2025-10-11 00:14:36 -0400

    conversegpt actually scrolls

--- ssu-fam-jam-game-godot-project/Windows/Converse/converse_window.gd
+++ ssu-fam-jam-game-godot-project/Windows/Converse/converse_window.gd
@@ -19,11 +19,11 @@ func _on_line_edit_text_submitted(new_text: String) -> void:
 	var newchat = Label.new()
 	newchat.text = new_text
 	newchat.add_theme_color_override("font_color", Color(0.65, 0.65, 0.65, 1.0))
-	$CenterContainer/VBoxContainer.add_child(newchat)
+	$ScrollContainer/CenterContainer/VBoxContainer.add_child(newchat)
 	await get_tree().create_timer(0.5).timeout
 	var response = Label.new()
 	response.text = responses.pick_random()
 	response.add_theme_color_override("font_color", Color(1.0, 1.0, 1.0, 1.0))
-	$CenterContainer/VBoxContainer.add_child(response)
-	$CenterContainer/VBoxContainer.move_child($CenterContainer/VBoxContainer/LineEdit, $CenterContainer/VBoxContainer.get_child_count()-1)
-	$CenterContainer/VBoxContainer/LineEdit.text = ""
+	$ScrollContainer/CenterContainer/VBoxContainer.add_child(response)
+	$ScrollContainer/CenterContainer/VBoxContainer.move_child($ScrollContainer/CenterContainer/VBoxContainer/LineEdit, $ScrollContainer/CenterContainer/VBoxContainer.get_child_count()-1)
+	$ScrollContainer/CenterContainer/VBoxContainer/LineEdit.text = ""
