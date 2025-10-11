1160e94de2204a4e5f8c4a69865ba2b2aab12191
AdkinsC0211 <98196686+ConnNAND@users.noreply.github.com>
2025-10-11 17:24:18 -0400

    removed lame warning

--- ssu-fam-jam-game-godot-project/Desktop/BlueScreen/blue_screen.gd
+++ ssu-fam-jam-game-godot-project/Desktop/BlueScreen/blue_screen.gd
@@ -1,5 +1,5 @@
 extends ColorRect
 
-func _unhandled_key_input(event: InputEvent) -> void:
+func _unhandled_key_input(_event: InputEvent) -> void:
 	GlobalData.reset()
 	get_tree().change_scene_to_file("res://Desktop/desktop.tscn")
