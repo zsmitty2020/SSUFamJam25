fae11a059964925879b19aed7734414f48a42a7f
AestheticYellow <kfkouba5231@gmail.com>
2025-10-11 15:34:07 -0400

    added randow spawn and button function

--- ssu-fam-jam-game-godot-project/Windows/Ball_Bouncer/Ball.gd
+++ ssu-fam-jam-game-godot-project/Windows/Ball_Bouncer/Ball.gd
@@ -1,12 +1,16 @@
 extends Area2D
 
 var screen_size
-var big = rotation
+var speed_upgrade_amount = 0
+var value_upgrade_amount = 0
 
 @export var speed = 0.75
-
+var bounce_value = 0.01
 
 func _on_ball_ready() -> void:
+	position.x = randi_range(10,320)
+	position.y = randi_range(10,320)
+	rotation = randi_range(10,320)
 	screen_size = get_viewport_rect().size
 
 
@@ -18,22 +22,22 @@ func _process(_delta: float) -> void:
 	if position.x > 490:
 		position.x = 490
 		dx *= -1
-		GlobalData.balance += 0.01
+		GlobalData.balance += bounce_value
 
 	if position.x < 0:
 		position.x = 0
 		dx *= -1
-		GlobalData.balance += 0.01
+		GlobalData.balance += bounce_value
 
 	if position.y > screen_size.y:
 		position.y = screen_size.y
 		dy *= -1
-		GlobalData.balance += 0.01
+		GlobalData.balance += bounce_value
 
 	if position.y < 0:
 		position.y = 0
 		dy *= -1
-		GlobalData.balance += 0.01
+		GlobalData.balance += bounce_value
 	rotation_degrees = rad_to_deg(atan2(-dy, dx))
 	position = position.clamp(Vector2.ZERO, screen_size)
 	GlobalData.balance = snappedf(GlobalData.balance,0.01)
--- ssu-fam-jam-game-godot-project/Windows/Ball_Bouncer/ball_bouncer.gd
+++ ssu-fam-jam-game-godot-project/Windows/Ball_Bouncer/ball_bouncer.gd
@@ -1,6 +1,17 @@
 extends TextureRect
 
 var balls = preload("res://Windows/Ball_Bouncer/ball.tscn")
+var create_ball_price = 75.0
+var speed_price = 50.0
+var bounce_value_price = 150.0
+var ball_amount = 0
+
+
+
+func _process(_delta: float) -> void:
+	$Purchase/PurchaseBallButton.text = "Purchase Ball: $" + "%.2f"%create_ball_price
+	$Purchase/PurchaseSpeedButton.text = "Purchase Speed: $" + "%.2f"%speed_price
+	$Purchase/PurchaseBounceValueButton.text = "Purchase Bounce \nValue: $" + "%.2f"%bounce_value_price
 
 func _on_ball_bouncer_close_requested() -> void:
 	GlobalData.open_tabs.erase("Ball_Bouncer")
@@ -11,4 +22,28 @@ func create_ball():
 	$sack.add_child(spawn_balls)
 
 func _on_button_pressed() -> void:
-	create_ball()
+	if ball_amount < 5:
+		if GlobalData.balance >= create_ball_price:
+			GlobalData.balance -= create_ball_price
+			create_ball()
+			create_ball_price += 75.0
+			ball_amount += 1
+
+func _on_purchase_speed_button_pressed() -> void:
+	for i in $sack.get_children():
+		if i.speed_upgrade_amount < 5:
+			if GlobalData.balance >= speed_price:
+				GlobalData.balance -= speed_price
+				i.speed += 0.75
+				i.speed_upgrade_amount += 1
+				speed_price += 50.0
+
+
+func _on_purchase_bounce_value_button_pressed() -> void:
+	for i in $sack.get_children():
+		if i.value_upgrade_amount < 10:
+			if GlobalData.balance >= bounce_value_price:
+				GlobalData.balance -= bounce_value_price
+				i.bounce_value += 0.01
+				i.value_upgrade_amount += 1
+				bounce_value_price += 150.0
