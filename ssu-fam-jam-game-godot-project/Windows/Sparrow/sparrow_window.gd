6bc1c4cd39b0411078466d1b94602d7736b6cfda
AdkinsC0211 <98196686+ConnNAND@users.noreply.github.com>
2025-10-11 19:54:25 -0400

    added stock market

--- ssu-fam-jam-game-godot-project/Windows/Sparrow/sparrow_window.gd
+++ ssu-fam-jam-game-godot-project/Windows/Sparrow/sparrow_window.gd
@@ -1,6 +1,49 @@
 extends TextureRect
 
 
+var stocks = []
+var stockIndex = 0
+
+func _ready() -> void:
+	stocks = GlobalData.stocks.keys()
+	$Stock.current_stock = stocks[stockIndex]
+
+
+func _process(_delta: float) -> void:
+	$StockTitle.text = "Current Stock: " + $Stock.current_stock
+	$Price.text = "Price: $%.2f"%$Stock.price
+	if GlobalData.inventory.has(stocks[stockIndex]):
+		$VBoxContainer/Owned.text = "Owned: " + str(GlobalData.inventory[stocks[stockIndex]])
+	else:
+		$VBoxContainer/Owned.text = "Owned: 0"
+
+
 func _on_sparrow_window_close_requested() -> void:
 	GlobalData.open_tabs.erase("sparrow")
 	get_parent().queue_free()
+
+
+func _on__pressed() -> void:
+	stockIndex = wrapi(stockIndex+1, 0, stocks.size()-1)
+	$Stock.current_stock = stocks[stockIndex]
+
+
+func _on_left_pressed() -> void:
+	stockIndex = wrapi(stockIndex-1, 0, stocks.size()-1)
+	$Stock.current_stock = stocks[stockIndex]
+
+
+func _on_buy_pressed() -> void:
+	if GlobalData.balance >= $Stock.price:
+		GlobalData.balance -= $Stock.price
+		if GlobalData.inventory.has(stocks[stockIndex]):
+			GlobalData.inventory[stocks[stockIndex]] += 1
+		else:
+			GlobalData.inventory[stocks[stockIndex]] = 1
+
+
+func _on_sell_pressed() -> void:
+	if GlobalData.inventory.has(stocks[stockIndex]):
+		if GlobalData.inventory[stocks[stockIndex]] > 0:
+			GlobalData.inventory[stocks[stockIndex]] -= 1
+			GlobalData.balance += $Stock.price
---
+++ ssu-fam-jam-game-godot-project/Windows/Sparrow/stock.gd
@@ -0,0 +1,35 @@
+extends Line2D
+
+
+var stockpeak = 200
+var stockmin = 0.01
+
+var volatility = randf_range(0.1, 10)
+var volatility_cycle = randf_range(1, 10)
+
+var stock_time = 0.25
+var stock_timer = stock_time
+
+var current_stock = ""
+
+var price_history = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+var price = 0
+
+
+func _ready() -> void:
+	current_stock = GlobalData.stocks.keys()[0]
+
+
+func _process(delta: float) -> void:
+	stock_timer -= delta
+	if stock_timer < 0:
+		stock_timer = stock_time
+		display_stock(current_stock)
+
+func display_stock(stock_name:String):
+	if stock_name != "":
+		price_history = GlobalData.stocks[stock_name]
+		price = price_history[-1]
+		clear_points()
+		for i in range(price_history.size()):
+			add_point(Vector2((get_window().size.x/24.0)*i, get_window().size.y*(1-(price_history[i]-stockmin)/(stockpeak-stockmin))))
