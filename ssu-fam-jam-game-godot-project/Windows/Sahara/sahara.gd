67ae851ecfd3405bdf5ef70cfe8628a084dc9ba2
ThePeachyOne <mccoold7@gmail.com>
2025-10-12 00:37:55 -0400

    Sahara workable
    Scaling not 100% perfect with window resizes

--- ssu-fam-jam-game-godot-project/Windows/Sahara/sahara.gd
+++ ssu-fam-jam-game-godot-project/Windows/Sahara/sahara.gd
@@ -6,12 +6,11 @@ func _ready() -> void:
 	get_tree().root.size_changed.connect(_on_viewport_size_changed)
 	_on_viewport_size_changed()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 func _on_viewport_size_changed():
 	var viewport_size = get_tree().root.size
 	set_size(viewport_size)
 	#print("thing")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
