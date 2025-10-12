8547ca06b39d93a992726ca81ab9e2d363b81481
AdkinsC0211 <98196686+ConnNAND@users.noreply.github.com>
2025-10-12 00:11:31 -0400

    added banner ads

--- ssu-fam-jam-game-godot-project/Windows/AdView/adview_window.gd
+++ ssu-fam-jam-game-godot-project/Windows/AdView/adview_window.gd
@@ -23,6 +23,12 @@ func _process(delta: float) -> void:
 		if not just_maxed:
 			just_maxed = true
 			$ImageAd.visible = true
+			var dir := DirAccess.open("res://Windows/AdView/Banners/")
+			var filenames := dir.get_files()
+			var random_file:String = ""
+			while not random_file.ends_with(".jpg"):
+				random_file = filenames[randi()%filenames.size()]
+			$ImageAd.texture = load("res://Windows/AdView/Banners/"+random_file)
 			$ImageAd/Close.global_position = Vector2(randi_range(10,get_window().size.x-10), randi_range(10,get_window().size.y-10))
 	else:
 		if $VideoStreamPlayer.paused:
