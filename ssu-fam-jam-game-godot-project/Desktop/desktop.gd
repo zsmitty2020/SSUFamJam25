11b8298710a04bc4f051e162de5a849320af8f2c
AdkinsC0211 <98196686+ConnNAND@users.noreply.github.com>
2025-10-11 21:49:17 -0400

    added boot-up sound

--- ssu-fam-jam-game-godot-project/Desktop/desktop.gd
+++ ssu-fam-jam-game-godot-project/Desktop/desktop.gd
@@ -5,10 +5,11 @@ extends TextureRect
 func _ready() -> void:
 	get_tree().root.size_changed.connect(_on_viewport_size_changed)
 	_on_viewport_size_changed()
+	$AnimationPlayer.play("Fade-In")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 func _on_viewport_size_changed():
